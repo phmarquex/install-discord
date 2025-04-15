4bd97e2d359bfeb346161092a71d15ce0cacb990
Paulo Marques <paulo.marques@ciadeestagios.com.br>
2025-04-15 11:43:53 -0300

    feat: mute curl, add flow messages

--- install.sh
+++ install.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env sh
 
-curl -L https://discord.com/api/download?platform=linux -o /tmp/discord.deb
+echo 'Baixando última versão do Discord...'
+curl -L https://discord.com/api/download?platform=linux -o /tmp/discord.deb >/dev/null 2>&1
+echo 'Instalando deb...'
 sudo apt install -y /tmp/discord.deb
