3457aab722180c0be6609be06a28a8705e9e8bf3
Paulo Marques <paulo.marques@ciadeestagios.com.br>
2025-04-15 11:53:30 -0300

    feat: close discord before installation

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env sh
 
+echo 'Fechando discord...'
+pkill Discord && sleep 5
 echo 'Baixando última versão do Discord...'
 curl -L https://discord.com/api/download?platform=linux -o /tmp/discord.deb >/dev/null 2>&1
 echo 'Instalando deb...'
