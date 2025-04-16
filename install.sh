c71cab582a36ef89774026d3cbcd87cec44d5e7f
Paulo Marques <paulo.marques@intelligenti.com.br>
2025-04-15 22:15:03 -0300

    feat: Clean all leftovers

--- install.sh
+++ install.sh
@@ -6,3 +6,5 @@ echo 'Baixando última versão do Discord...'
 curl -L https://discord.com/api/download?platform=linux -o /tmp/discord.deb >/dev/null 2>&1
 echo 'Instalando deb...'
 sudo apt install -y /tmp/discord.deb
+echo 'Cleaning up...'
+rm -f /tmp/discord.deb
